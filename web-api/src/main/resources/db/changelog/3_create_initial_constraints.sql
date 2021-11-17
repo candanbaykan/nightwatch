2dace08e0323f3d3e6cf6178db81351ef540ca5a
Candan Baykan <candanbaykan@yandex.com>
2021-11-17 22:50:18 +0300

    Created initial constraints for the database

---
+++ web-api/src/main/resources/db/changelog/3_create_initial_constraints.sql
@@ -0,0 +1,27 @@
+ALTER TABLE public.watch
+    ADD CONSTRAINT watch_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id);
+
+ALTER TABLE public."user"
+    ADD CONSTRAINT user_fk FOREIGN KEY (role_id) REFERENCES public."role" (id);
+ALTER TABLE public."user"
+    ADD CONSTRAINT user_un UNIQUE (username);
+
+
+ALTER TABLE public.preferred_day
+    ADD CONSTRAINT preferred_day_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id);
+
+ALTER TABLE public.off_day
+    ADD CONSTRAINT off_day_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id);
+
+ALTER TABLE public.manager
+    ADD CONSTRAINT manager_fk FOREIGN KEY (user_id) REFERENCES public."user" (id);
+
+ALTER TABLE public.employee
+    ADD CONSTRAINT employee_fk FOREIGN KEY (department_id) REFERENCES public.department (id);
+ALTER TABLE public.employee
+    ADD CONSTRAINT employee_fk_1 FOREIGN KEY (rank_id) REFERENCES public."rank" (id);
+ALTER TABLE public.employee
+    ADD CONSTRAINT employee_fk_2 FOREIGN KEY (user_id) REFERENCES public."user" (id);
+
+ALTER TABLE public.department
+    ADD CONSTRAINT department_fk FOREIGN KEY (manager_id) REFERENCES public.manager (id);
