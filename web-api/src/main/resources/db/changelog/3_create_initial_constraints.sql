20865bebeb23a44df40b2f840cb270d4f71a9197
Candan Baykan <candanbaykan@yandex.com>
2021-11-20 17:30:24 +0300

    Added cascade deletes to foreign keys

--- web-api/src/main/resources/db/changelog/3_create_initial_constraints.sql
+++ web-api/src/main/resources/db/changelog/3_create_initial_constraints.sql
@@ -1,5 +1,5 @@
 ALTER TABLE public.watch
-    ADD CONSTRAINT watch_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id);
+    ADD CONSTRAINT watch_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id) ON DELETE CASCADE;
 
 ALTER TABLE public."user"
     ADD CONSTRAINT user_fk FOREIGN KEY (role_id) REFERENCES public."role" (id);
@@ -8,20 +8,20 @@ ALTER TABLE public."user"
 
 
 ALTER TABLE public.preferred_day
-    ADD CONSTRAINT preferred_day_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id);
+    ADD CONSTRAINT preferred_day_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id) ON DELETE CASCADE;
 
 ALTER TABLE public.off_day
-    ADD CONSTRAINT off_day_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id);
+    ADD CONSTRAINT off_day_fk FOREIGN KEY (employee_id) REFERENCES public.employee (id) ON DELETE CASCADE;
 
 ALTER TABLE public.manager
-    ADD CONSTRAINT manager_fk FOREIGN KEY (user_id) REFERENCES public."user" (id);
+    ADD CONSTRAINT manager_fk FOREIGN KEY (user_id) REFERENCES public."user" (id) ON DELETE CASCADE;
 
 ALTER TABLE public.employee
     ADD CONSTRAINT employee_fk FOREIGN KEY (department_id) REFERENCES public.department (id);
 ALTER TABLE public.employee
     ADD CONSTRAINT employee_fk_1 FOREIGN KEY (rank_id) REFERENCES public."rank" (id);
 ALTER TABLE public.employee
-    ADD CONSTRAINT employee_fk_2 FOREIGN KEY (user_id) REFERENCES public."user" (id);
+    ADD CONSTRAINT employee_fk_2 FOREIGN KEY (user_id) REFERENCES public."user" (id) ON DELETE CASCADE;
 
 ALTER TABLE public.department
     ADD CONSTRAINT department_fk FOREIGN KEY (manager_id) REFERENCES public.manager (id);
