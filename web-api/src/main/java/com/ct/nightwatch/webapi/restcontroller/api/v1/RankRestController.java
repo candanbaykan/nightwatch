416c91d5d854b612d5133f5ecf42ba7c94677fa3
Candan Baykan <candanbaykan@yandex.com>
2021-12-03 20:32:25 +0300

    RankRestController created

---
+++ web-api/src/main/java/com/ct/nightwatch/webapi/restcontroller/api/v1/RankRestController.java
@@ -0,0 +1,56 @@
+package com.ct.nightwatch.webapi.restcontroller.api.v1;
+
+import com.ct.nightwatch.webapi.restcontroller.api.utility.ApiUtility;
+import com.ct.nightwatch.webapi.service.RankService;
+import com.ct.nightwatch.webapi.service.dto.RankRequest;
+import com.ct.nightwatch.webapi.service.dto.RankSummary;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/ranks")
+@CrossOrigin("*")
+public class RankRestController {
+
+    private final RankService rankService;
+
+    public RankRestController(RankService rankService) {
+        this.rankService = rankService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RankSummary>> getAll() {
+        return ResponseEntity.ok(rankService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RankSummary> getById(@PathVariable Long id) {
+        return rankService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> post(@RequestBody RankRequest rankRequest) {
+        Long id = rankService.save(rankRequest);
+        URI uri = ApiUtility.createLocationUri(id);
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RankSummary> putById(@PathVariable Long id, @RequestBody RankRequest rankRequest) {
+        return rankService.updateById(id, rankRequest)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        rankService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
