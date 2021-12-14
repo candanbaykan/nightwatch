f158d43cece602c1c5c77d0d574e6bd8e4751a0d
Candan Baykan <candanbaykan@yandex.com>
2021-12-15 01:45:21 +0300

    StringTrimmer improved

--- web-api/src/main/java/com/ct/nightwatch/webapi/service/trimmer/StringTrimmer.java
+++ web-api/src/main/java/com/ct/nightwatch/webapi/service/trimmer/StringTrimmer.java
@@ -1,55 +1,49 @@
 package com.ct.nightwatch.webapi.service.trimmer;
 
+import com.ct.nightwatch.webapi.service.trimmer.annotation.NotTrimmable;
+import com.ct.nightwatch.webapi.service.trimmer.annotation.Trimmable;
+import com.ct.nightwatch.webapi.service.trimmer.exception.StringTrimmerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.beans.BeanInfo;
-import java.beans.Introspector;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.lang.reflect.Field;
 
 public class StringTrimmer {
 
     private static final Logger logger = LoggerFactory.getLogger(StringTrimmer.class);
 
     public static <T> void trim(T object) {
-        trim(object, null);
+        trim(object, object.getClass());
     }
 
-    public static <T> void trim(T object, Set<String> exclude) {
-        if (object == null)
-            return;
-
-        exclude = Optional.ofNullable(exclude)
-                .orElse(new HashSet<>());
-
+    private static <T> void trim(T object, Class<?> theClass) {
         try {
-            BeanInfo beanInfo = Introspector.getBeanInfo(object.getClass());
-            Set<String> finalExclude = exclude;
-            Arrays.stream(beanInfo.getPropertyDescriptors())
-                    .filter(propertyDescriptor -> {
-                        return propertyDescriptor.getPropertyType().isAssignableFrom(String.class) &&
-                                !finalExclude.contains(propertyDescriptor.getName());
-                    })
-                    .forEach(propertyDescriptor -> {
-                        try {
-                            String value = Optional.ofNullable(
-                                            (String) propertyDescriptor.getReadMethod().invoke(object)
-                                    )
-                                    .map(String::trim)
-                                    .orElse(null);
+            if (object == null || theClass == null || theClass.equals(Object.class))
+                return;
 
-                            propertyDescriptor.getWriteMethod().invoke(object, value);
-                        } catch (IllegalAccessException | InvocationTargetException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
+            trim(object, theClass.getSuperclass());
+
+            if (!theClass.isAnnotationPresent(Trimmable.class))
+                return;
+
+
+            for (Field field : theClass.getDeclaredFields()) {
+                if (field.isAnnotationPresent(NotTrimmable.class))
+                    continue;
+
+                field.setAccessible(true);
+                Object fieldValue = field.get(object);
+                Class<?> fieldClass = fieldValue.getClass();
+
+                if (fieldClass.equals(String.class))
+                    field.set(object, ((String) fieldValue).trim());
+                else
+                    trim(fieldValue, fieldClass);
+
+            }
         } catch (Exception e) {
             logger.error("An error occurred during trimming", e);
-            throw new RuntimeException(e);
+            throw new StringTrimmerException(e);
         }
     }
 
---
+++ web-api/src/main/java/com/ct/nightwatch/webapi/service/trimmer/exception/StringTrimmerException.java
@@ -0,0 +1,12 @@
+package com.ct.nightwatch.webapi.service.trimmer.exception;
+
+public class StringTrimmerException extends RuntimeException {
+
+    public StringTrimmerException() {
+    }
+
+    public StringTrimmerException(Throwable cause) {
+        super(cause);
+    }
+
+}
