b9c128756d341ea81078ea101e4953bef8cc2085
Candan Baykan <candanbaykan@yandex.com>
2022-01-27 23:19:14 +0300

    Resolve userId parameter bug in DefaultEmployeeService

--- web-api/src/main/java/com/ct/nightwatch/webapi/service/implementation/DefaultEmployeeService.java
+++ web-api/src/main/java/com/ct/nightwatch/webapi/service/implementation/DefaultEmployeeService.java
@@ -40,7 +40,7 @@ public class DefaultEmployeeService implements EmployeeService {
             "@authService.isAdmin() || " +
                     "(#parameters['userId'] != null && " +
                     "(@authService.isUserManager(#parameters['userId']) || " +
-                    "@authService.isEqualEmployee(#parameters['userId'])))"
+                    "@authService.isEqualUser(#parameters['userId'])))"
     )
     public List<EmployeeListItem> findAll(Map<String, String> parameters) {
         EmployeeSpecification specification = new EmployeeSpecification(parameters);
